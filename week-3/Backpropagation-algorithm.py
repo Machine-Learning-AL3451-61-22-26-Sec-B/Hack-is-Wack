d18e67be5144ab018b3e0cdd85b0f27f6d08489b
Nivetha22aib13 <126764030+Nivetha22aib13@users.noreply.github.com>
2024-06-08 19:44:55 -0700

    Update Backpropagation-algorithm.py

--- week-3/Backpropagation-algorithm.py
+++ week-3/Backpropagation-algorithm.py
@@ -1,59 +1,64 @@
-import streamlit as st
 import numpy as np
+import streamlit as st
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class NeuralNetwork:
+    def __init__(self):
+        # Parameters
+        self.input_size = 2
+        self.output_size = 1
+        self.hidden_size = 3
+        
+        # Weights
+        self.W1 = np.random.randn(self.input_size, self.hidden_size)
+        self.W2 = np.random.randn(self.hidden_size, self.output_size)
 
-def sigmoid_grad(x):
-    return x * (1 - x)
+    def forward(self, X):
+        # Forward propagation through the network
+        self.z = np.dot(X, self.W1)
+        self.z2 = self.sigmoid(self.z)
+        self.z3 = np.dot(self.z2, self.W2)
+        o = self.sigmoid(self.z3)
+        return o 
 
-@st.cache
-def neural_network(X, y, epoch=1000, eta=0.2):
-    input_neurons = X.shape[1]
-    hidden_neurons = 3
-    output_neurons = 1
+    def backward(self, X, y, o):
+        # Backward propagate through the network
+        self.o_error = y - o
+        self.o_delta = self.o_error * self.sigmoid_prime(o)
+        self.z2_error = self.o_delta.dot(self.W2.T)
+        self.z2_delta = self.z2_error * self.sigmoid_prime(self.z2)
+        self.W1 += X.T.dot(self.z2_delta)
+        self.W2 += self.z2.T.dot(self.o_delta)
 
-    # Variable initialization
-    wh = np.random.uniform(size=(input_neurons, hidden_neurons))  # 2x3
-    bh = np.random.uniform(size=(1, hidden_neurons))  # 1x3
-    wout = np.random.uniform(size=(hidden_neurons, output_neurons))  # 1x1
-    bout = np.random.uniform(size=(1, output_neurons))
+    def train(self, X, y):
+        # Training function
+        o = self.forward(X)
+        self.backward(X, y, o)
 
-    for i in range(epoch):
-        h_ip = np.dot(X, wh) + bh
-        h_act = sigmoid(h_ip)
-        o_ip = np.dot(h_act, wout) + bout
-        output = sigmoid(o_ip)
+    def sigmoid(self, s):
+        # Activation function
+        return 1 / (1 + np.exp(-s))
 
-        Eo = y - output
-        outgrad = sigmoid_grad(output)
-        d_output = Eo * outgrad
+    def sigmoid_prime(self, s):
+        # Derivative of sigmoid
+        return s * (1 - s)
 
-        Eh = d_output.dot(wout.T)
-        hiddengrad = sigmoid_grad(h_act)
-        d_hidden = Eh * hiddengrad
+    def evaluate(self, X):
+        # Evaluation function
+        return self.forward(X)
 
-        wout += h_act.T.dot(d_output) * eta
-        wh += X.T.dot(d_hidden) * eta
+if __name__ == "__main__":
+    # Sample data
+    X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
+    y = np.array(([92], [86], [89]), dtype=float)
 
-    return output
+    # Scale units
+    X = X / np.amax(X, axis=0)
+    y = y / 100  # Max test score is 100
 
-st.title("Neural Network Output")
-
-# Input data
-X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
-
-# Normalize data
-X = X / np.amax(X, axis=0)
-y = y / 100
-
-# Get predicted output
-predicted_output = neural_network(X, y)
-
-st.write("Normalized Input:")
-st.write(X)
-st.write("Actual Output:")
-st.write(y)
-st.write("Predicted Output:")
-st.write(predicted_output)
+    # Create and train neural network
+    NN = NeuralNetwork()
+    print("\nInput:\n", X)
+    print("\nActual Output:\n", y)
+    print("\nPredicted Output:\n", NN.evaluate(X))
+    print("\nLoss:\n", np.mean(np.square(y - NN.evaluate(X))))  # Mean squared loss
+    NN.train(X, y)
